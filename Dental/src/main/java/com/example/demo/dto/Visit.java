4b1dd5b2e459fdb19b6c6196f4e54859b71be5d4
Javier Prz <58307741+jvprz@users.noreply.github.com>
2023-05-07 12:45:48 +0200

    Visit entity

--- Dental/src/main/java/com/example/demo/dto/Visit.java
+++ Dental/src/main/java/com/example/demo/dto/Visit.java
@@ -1,28 +1,88 @@
 package com.example.demo.dto;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "odontologists")
+@Table(name = "visits")
 public class Visit {
 
 	// Attributes
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String dni;
-	@Column(name = "name_surname")
-	private String names;
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private Client client;
+	@ManyToOne
+	@JoinColumn(name = "odontologist_id")
+	private Odontologist odontologist;
+	@Column(name = "procedure_type")
+	private String procedure;
+	@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime scheduled;
+	private double price;
 
-		private String phone_number;
+	// Constructors
+	public Visit() {}
 
-		private String email;
+	public Visit(Long id, Client client, Odontologist odontologist, String procedure, LocalDateTime scheduled, double price) {
+		this.id = id;
+		this.client = client;
+		this.odontologist = odontologist;
+		this.procedure = procedure;
+		this.scheduled = scheduled;
+		this.price = price;
+	}
 
-		private String speciality;
+	// Getters
+	public Long getId() {
+		return id;
+	}
 
-		private int medical_license_no;
+	public Client getClient() {
+		return client;
+	}
 
-		@OneToMany(mappedBy = "odontologist")
-		private List<Visit> visit;
+	public Odontologist getOdontologist() {
+		return odontologist;
+	}
 
+	public String getProcedure() {
+		return procedure;
+	}
+
+	public LocalDateTime getScheduled() {
+		return scheduled;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	// Setters
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public void setOdontologist(Odontologist odontologist) {
+		this.odontologist = odontologist;
+	}
+
+	public void setProcedure(String procedure) {
+		this.procedure = procedure;
+	}
+
+	public void setScheduled(LocalDateTime scheduled) {
+		this.scheduled = scheduled;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
 }
